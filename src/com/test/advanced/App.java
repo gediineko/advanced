2f436215a597fc6df330dd272a612f0f879e6a9d
Geraldine Golong <geraldinegolong@gmail.com>
2016-07-07 16:58:36 +0800

    Added App.java

---
+++ src/com/test/advanced/App.java
@@ -0,0 +1,14 @@
+package com.test.advanced;
+public class App{
+    public App(){
+
+    }
+    public static void main(String[] args){
+        new App().run();
+    }
+    public void run(){
+        
+
+    }
+    
+}
